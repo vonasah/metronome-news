6ec055fb021fe4ba51bcbf8c91d8d195ff03e910
vonasah <vonasahh1@gmail.com>
2026-05-06 10:54:17 +0500

    Fix RSS feed - remove broken stylesheet

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,7 +8,7 @@ export async function GET(context) {
 	// Сортируем по дате — самые свежие сверху
 	articles.sort((a, b) => b.data.publishDate.valueOf() - a.data.publishDate.valueOf());
 
-	// Берём первые 30 статей (стандарт для RSS feeds)
+	// Берём первые 30 статей
 	const recentArticles = articles.slice(0, 30);
 
 	return rss({
@@ -22,7 +22,6 @@ export async function GET(context) {
 			author: article.data.author + ' (raptor@metronome.news)',
 			categories: [article.data.category, ...(article.data.tags || [])],
 			link: `/article/${article.id}/`,
-			// Полный URL картинки для лучшей совместимости с агрегаторами
 			...(article.data.image && {
 				enclosure: {
 					url: new URL(article.data.image, context.site).href,
@@ -30,28 +29,13 @@ export async function GET(context) {
 					type: 'image/jpeg'
 				}
 			}),
-			customData: article.data.image
-				? `<media:content url="${new URL(article.data.image, context.site).href}" medium="image" />`
-				: '',
 		})),
-		// XML namespace для media (картинки в RSS)
-		xmlns: {
-			media: 'http://search.yahoo.com/mrss/',
-		},
-		// Stylesheet для красивого отображения если кто-то откроет XML в браузере
-		stylesheet: '/rss-styles.xsl',
-		// Дополнительные метаданные
 		customData: `
 			<language>en-us</language>
 			<copyright>© 2026 Metronome News. All rights reserved.</copyright>
 			<managingEditor>contact@metronome.news (raptor)</managingEditor>
 			<webMaster>contact@metronome.news (raptor)</webMaster>
 			<ttl>60</ttl>
-			<image>
-				<url>${context.site}favicon.svg</url>
-				<title>Metronome News</title>
-				<link>${context.site}</link>
-			</image>
 		`,
 	});
 }
